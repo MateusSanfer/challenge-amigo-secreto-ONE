456127e4fb9db019185123247b5a03af37b35d88
Mateus Sanfer <mateus.sanfer@gmail.com>
2025-01-23 23:12:53 -0300

    Implementa uma função para sortear os amigos

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
-let listaAmigosArray = [];
+let amigos = [];
 
 function adicionarAmigo() {
   let amigo = document.getElementById("amigo").value;
@@ -8,9 +8,9 @@ function adicionarAmigo() {
   if (amigo.trim().length === 0) {
     alert("Por favor, insira um nome.");
   } else {
-    listaAmigosArray.push(amigo);
+    amigos.push(amigo);
 
-    adicionarAmigosLista(listaAmigosArray);
+    adicionarAmigosLista(amigos);
   }
 
   limparCampo();
@@ -31,4 +31,19 @@ function adicionarAmigosLista(amigos) {
     li.textContent = amigos[i];
     lista.appendChild(li);
   }
+}
+
+function sortearAmigo() {
+  if (amigos.length === "") {
+    alert("Não há amigos para sortear!");
+    return;
+  }
+
+  const amigoAleatorio = Math.floor(Math.random() * amigos.length);
+  const amigoSorteado = amigos[amigoAleatorio];
+
+  let resultado = document.getElementById("resultado");
+  resultado.innerHTML = `Amigo Sorteado: ${amigoSorteado}`;
+  // let lista = document.getElementById("listaAmigos");
+  // lista.innerHTML = "";
 }
